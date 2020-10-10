00f48516f17d7d075fa7b81a80871e3d387c4e42
younus10s <younus.salman@yahoo.com>
2020-10-10 22:38:30 +0100

    updating js and css

--- time_website/web-clock/src/index.js
+++ time_website/web-clock/src/index.js
@@ -2,20 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Clock(){
-    const element = (
-        <div>
-        <h1 className="clock">{new Date().toLocaleTimeString()}</h1>
-        <h1 className="date">{new Date().toLocaleDateString()}</h1>
-
-        </div>
-      );
-      
-      ReactDOM.render(
-        element,
-        document.getElementById('root')
-      );
-    }
+class Clock extends React.Component{
+  constructor(probs){
+    super(probs);
+    this.state = {
+      time: new Date()
+    };
+  }
   
+  componentDidMount(){
+    {/* method to update timer*/}
+    this.Interval = setInterval(
+      () => {
+        this.setState({time: new Date()})
+      }, 1000
+    );
+  }
+
+  componentWillUnmount() {
+    {/* method to clear timer for not having memory leaked*/}
+    clearInterval(this.Interval);
+  }
+
+  render() {
+    var h = this.state.time.getHours()-1;
+    var min = this.state.time.getMinutes();
+    var sek = this.state.time.getSeconds();
+    return(
+      <div>
+      <h1 className="clock">{h}:{min}:{sek}</h1>
+      <h1 className="date">{new Date().toLocaleDateString()}</h1>
+
+      </div>
+    );
+  }
+    
+}
+
+class MainClass extends React.Component{
+  render(){
+    return (
+      <div className="main">
+        < Clock />
+      </div>
+    );
+  }
+}
+
   // ========================================
-  setInterval(Clock, 1000);
+ReactDOM.render(
+  <MainClass />,
+  document.getElementById('root')
+);
+  
